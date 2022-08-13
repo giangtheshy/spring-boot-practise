90486cb40b68ed06fa1bcdc7dcbb62422ee19256
Nguyen Truong Giang <72980294+giangtheshy@users.noreply.github.com>
2022-08-13 14:29:59 +0700

    change GeoPoint package

--- src/main/java/com/practise/elasticsearch/User.java
+++ src/main/java/com/practise/elasticsearch/User.java
@@ -5,7 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.elasticsearch.common.geo.GeoPoint;
+import org.springframework.data.elasticsearch.core.geo.GeoPoint;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.GeoPointField;
